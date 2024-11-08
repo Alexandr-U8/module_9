606beda409f359cdbfc728732a54fb9ee41baad8
Alexandr-U8 <utkin88nya@yandex.ru>
2024-11-08 16:54:43 +0300

    Add files via upload

---
+++ module_9_3.py
@@ -0,0 +1,17 @@
+first = ['Strings', 'Student', 'Computers']
+second = ['Строка', 'Урбан', 'Компьютер']
+
+# генераторная сборка, которая высчитывает разницу длин строк из списков first и second,
+# если их длины не равны. Для перебора строк попарно из двух списков используйте функцию zip.
+
+first_result = (len(ft) - len(sd) for (ft, sd) in zip(first, second) if len(ft) != len(sd))
+
+
+# генераторную сборку, которая содержит результаты сравнения длин строк в одинаковых позициях
+# из списков first и second. Составьте эту сборку НЕ используя функцию zip. Используйте функции
+# range и len.
+
+second_result = (len(first[i]) == len(second[i]) for i in range(len(first)))
+
+print(list(first_result))
+print(list(second_result))
